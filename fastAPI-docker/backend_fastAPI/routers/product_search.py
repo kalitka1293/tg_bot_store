708ed831854862f08788cd955f6ccfc43b1d807b
main pc <b@lol.com>
2025-05-16 17:50:14 +0300

    edit fix problem

--- fastAPI-docker/backend_fastAPI/routers/product_search.py
+++ fastAPI-docker/backend_fastAPI/routers/product_search.py
@@ -8,7 +8,7 @@ async def perform_search(query):
     client = Client('http://meilisearch:7700', 'rcN6lh9xlE4_vcL7yuEK5YOrLleh8tdMR2m1FVcvRe0')
     index = client.index('product')
     print(index, query, '<<<<')
-    result = await index.search(query=query, limit=5)
+    result = index.search(query=query)
     print(result)
     return result
 
