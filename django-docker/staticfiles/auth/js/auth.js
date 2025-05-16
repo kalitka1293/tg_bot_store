921a7dee9224a3c64a5d84bf72fbe8aead811448
main pc <b@lol.com>
2025-05-16 14:25:28 +0300

    create docker-compose command staticcollection, setting foler for static

---
+++ django-docker/staticfiles/auth/js/auth.js
@@ -0,0 +1,32 @@
+﻿
+
+document.addEventListener('DOMContentLoaded', () => {
+      (function () {
+    const data = 'dfhgibugsdf';
+    axios({
+        method: 'POST',
+        url: window.API_URLS.getToken,
+        withCredentials: true,
+        headers: {
+            'Content-type': 'application/json'
+        },
+        data: {
+            initData: window.INIT_DATA.initData
+        }
+    }).then(function (response) {
+        console.log(response)
+    }).catch(function (error) {
+        if (error.response && error.response.status === 500) {
+            window.location.replace(window.ERROR.error_authorization);
+            alert('Требуется авторизация');
+        } else {
+            console.error('Ошибка:', error.message);
+            if (error.response) {
+                console.error('Данные ошибки:', error.response.data);
+            }
+        }
+    });
+})();
+});
+
+
