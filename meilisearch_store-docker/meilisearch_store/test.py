b8012eea8cdcaf538688b34f4c8c6f93b82bdad1
main pc <b@lol.com>
2025-05-16 16:50:39 +0300

    edit fix problem

--- meilisearch_store-docker/meilisearch_store/test.py
+++ meilisearch_store-docker/meilisearch_store/test.py
@@ -1,6 +1,6 @@
 from database import get_db, Brand
 from dowload_data_meilisearch import add_all_data_product
-
+from app import client
 def test():
     print("Run test!")
     db = get_db()
@@ -22,7 +22,15 @@ def test():
     db.delete(brand_from_db)
     db.commit()
     print("test BD success")
-
+    print('update_searchable_attributes')
+    client.index.update_searchable_attributes([
+        "name",
+        "description",
+        "brand",
+        "country",
+        "group_product",
+        "type_equipment"
+    ])
     print('TEST download data bd in meilisearch')
     add_all_data_product('product')
     print('Success')
