953f1fb515eff13894e7fe9bd9a42ea1c6373396
main pc <b@lol.com>
2025-05-19 18:45:39 +0300

    go in prod

--- django-docker/website_store/website_store/settings.py
+++ django-docker/website_store/website_store/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env(
     # set casting, default value
-    DEBUG=(bool, True)
+    DEBUG=(bool, False)
 )
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
